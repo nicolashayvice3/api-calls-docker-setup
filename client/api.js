516fb17ad5411f8344a80d18cb3a367a1fa583e6
nicolas hayvice-caballe <nicolashayvice@gmail.com>
2021-10-26 17:15:57 +1300

    inital apis done

---
+++ client/api.js
@@ -0,0 +1,30 @@
+import request from 'superagent'
+
+const rootUrl = '/api/v1'
+
+export function getCows () {
+    return request.get(rootUrl)
+    .then(res => {
+      return res.body
+    })
+    .catch(e => console.log(e))
+}
+
+export function sendCow (newCow) {
+    return request.post(rootUrl)
+    .send(newCow)
+    .then(res => {
+      return null
+    })
+    .catch(e => console.log(e))
+}
+
+export function updateCow (id, updatedCow) {
+    return request
+    .patch(rootUrl + `/${id}`)
+    .send(updatedCow)
+    .then(res => {
+      return res.body
+    })
+    .catch(e => console.log(e))
+}
